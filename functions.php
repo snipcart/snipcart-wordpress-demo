1a3791147ad6fec4281b2878f94ec4503c2ef69b
yanickouellet <yanickouellet555@gmail.com>
2016-04-29 10:51:25 -0400

    Step 3.1: Load Snipcart script and stylesheet

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
 add_action( 'wp_enqueue_scripts', 'snipcart_enqueue_styles' );
 function snipcart_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'snipcart-style', 'https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css');
 }
