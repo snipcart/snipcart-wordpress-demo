9a10259826b3ca83aef6eb74a513b08794d9ac6a
yanickouellet <yanickouellet555@gmail.com>
2016-04-29 10:58:58 -0400

    Step 3.2: Add Snipcart buy button

--- single-product.php
+++ single-product.php
@@ -24,8 +24,20 @@ get_header(); ?>
                     price: <strong><?php echo get_post_meta($id, 'price')[0]; ?>$</strong> <br />
                     inventory: <strong><?php echo get_post_meta($id, 'inventory')[0]; ?> left</strong>
                 </div>
-
                 <div class="snipcart-button-wrapper">
+                    <button
+                        class="snipcart-add-item"
+                        data-item-id="<?php echo get_post_meta($id, 'id')[0]; ?>"
+                        data-item-name="<?php echo get_the_title(); ?>"
+                        data-item-price="<?php echo get_post_meta($id, 'price')[0]; ?>"
+                        data-item-max-quantity="<?php echo get_post_meta($id, 'inventory')[0]; ?>"
+                        data-item-url="<?php echo get_the_permalink(); ?>"
+                        data-item-image="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($id), 'single-post-thumbnail')[0]?>"
+                    >
+                        Buy
+                    </button>
+                </div>
+
             </article>
             <?php
 			// End of the loop.
